32a0341ebd1d9a2bddf20fd17ecde9be7bf806c7
halfstorm <halfstorm@nexigate.win>
2026-03-30 13:03:00 -0500

    Fix pig texture size on 1.20.1/1.21.1

--- common/src/main/java/win/demistorm/visual_health/client/entitymappings/TextureSizeIndex.java
+++ common/src/main/java/win/demistorm/visual_health/client/entitymappings/TextureSizeIndex.java
@@ -35,7 +35,7 @@ public class TextureSizeIndex {
         TEXTURE_SIZES.put(EntityType.MULE, new TextureSize(64, 64));
         TEXTURE_SIZES.put(EntityType.OCELOT, new TextureSize(64, 32));
         TEXTURE_SIZES.put(EntityType.PARROT, new TextureSize(32, 32));
-        TEXTURE_SIZES.put(EntityType.PIG, new TextureSize(64, 64));
+        TEXTURE_SIZES.put(EntityType.PIG, new TextureSize(64, 32));
         TEXTURE_SIZES.put(EntityType.RABBIT, new TextureSize(64, 32));
         TEXTURE_SIZES.put(EntityType.SHEEP, new TextureSize(64, 32));
         TEXTURE_SIZES.put(EntityType.SKELETON_HORSE, new TextureSize(64, 64));
