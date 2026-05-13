540a45a03756c2f13805e141d7b85ef206455768
halfstorm <halfstorm@nexigate.win>
2026-05-13 12:05:46 -0500

    Skip emissive RenderLayer rendering when Physics Mob creates ragdoll

--- common/src/main/java/win/demistorm/visual_health/client/renderer/DamageOverlayLayer.java
+++ common/src/main/java/win/demistorm/visual_health/client/renderer/DamageOverlayLayer.java
@@ -40,6 +40,10 @@ public class DamageOverlayLayer<T extends LivingEntity, M extends EntityModel<T>
             return;
         }
 
+        if (win.demistorm.visual_health.client.compat.PhysicsModBridge.getCapturingEntity() != null) {
+            return;
+        }
+
         EntityDamageColors.DamageOverride override =
                 EntityDamageColors.getOverride(entity.getType());
 
