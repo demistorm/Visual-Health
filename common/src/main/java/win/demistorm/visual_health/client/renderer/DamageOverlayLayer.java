8d0d70e4783154512962c39fcc90d3e92a852038
halfstorm <halfstorm@nexigate.win>
2026-05-22 13:39:22 -0500

    Fixes regressions on DamageOverlayLayer rendering (1.21.11) Issues: Config screen text (needs alpha update)

--- common/src/main/java/win/demistorm/visual_health/client/renderer/DamageOverlayLayer.java
+++ common/src/main/java/win/demistorm/visual_health/client/renderer/DamageOverlayLayer.java
@@ -78,6 +78,8 @@ public class DamageOverlayLayer<S extends LivingEntityRenderState, M extends Ent
 
             poseStack.pushPose();
 
+            submitNodeCollector.order(0).submitModel(model, entityRenderState, poseStack, renderType,
+                    finalPackedLight, overlay, -1, null, 0, null);
         if (isEmissive) {
             renderType = RenderTypes.entityTranslucentEmissive(woundTexture);
             finalPackedLight = 15728880;
