be87ffd3f7443989fb46c0b37820e80be259254c
halfstorm <halfstorm@nexigate.win>
2026-05-26 10:13:10 -0500

    Update magma cube damage color

--- common/src/main/java/win/demistorm/visual_health/client/entitymappings/EntityDamageColors.java
+++ common/src/main/java/win/demistorm/visual_health/client/entitymappings/EntityDamageColors.java
@@ -56,7 +56,7 @@ public class EntityDamageColors {
         OC(BREEZE, "c2c2c2", false);
         OC(GUARDIAN, "2323c1", false);
         OC(ELDER_GUARDIAN, "2323c1", false);
-        OC(MAGMA_CUBE, "ddaf13", false);
+        OC(MAGMA_CUBE, "8d2f00", false);
         OC(SHULKER, "ae6984", false);
         OC(SLIME, "9db45f", false);
         OC(WARDEN, "008388", false);
