4119dcdc9221f50afbe1abeb9f0916f686c0bbbf
halfstorm <halfstorm@nexigate.win>
2026-06-01 22:25:30 -0500

    Update CHANGELOG.md and disable Epic Fight's healthbars from damage

--- common/src/main/java/win/demistorm/visual_health/client/renderer/BufferSourceSwapHelper.java
+++ common/src/main/java/win/demistorm/visual_health/client/renderer/BufferSourceSwapHelper.java
@@ -122,6 +122,8 @@ public final class BufferSourceSwapHelper {
 
         if (path.startsWith("default/")) return true;
 
+        if (texture.getNamespace().equals("epicfight") && path.contains("/gui/")) return true;
+
         return texture.getNamespace().equals("visualhealth") && path.startsWith("dynamic/");
     }
 }
