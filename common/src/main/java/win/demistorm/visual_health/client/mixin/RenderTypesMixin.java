2f620f087b61327dd2e6475f8c7801a0c37573cf
halfstorm <halfstorm@nexigate.win>
2026-09-20 18:45:46 -0500

    Add entityCutoutCull RenderType

--- common/src/main/java/win/demistorm/visual_health/client/mixin/RenderTypesMixin.java
+++ common/src/main/java/win/demistorm/visual_health/client/mixin/RenderTypesMixin.java
@@ -21,6 +21,7 @@ public class RenderTypesMixin {
                     "endCrystalBeam",
                     "entityTranslucentCullItemTarget",
                     "entityCutout",
+                    "entityCutoutCull",
                     "entityCutoutDissolve",
                     "armorCutoutNoCull",
                     "entityShadow",
